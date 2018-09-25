690b7d4661cf2ee0052f8769a06c79183a1f8a08
Anand_Swaroop <prashu.swaroop@gmail.com>
2018-09-26 02:11:07 +0530

    In the middle

---
+++ db-course-4.py
@@ -0,0 +1,34 @@
+# Definitions
+# Class - a template​
+# Method or Message - A defined capability of a class ​
+# Field or attribute- A bit of data in a class​
+# Object or Instance - A particular instance of a class
+
+# Terminology: Class​
+# Defines the abstract characteristics of a thing (object), including the thing's characteristics (its attributes, fields or properties) and the thing's behaviors (the things it can do, or methods, operations or features). One might say that a class is a blueprint or factory that describes the nature of something. For example, the class Dog would consist of traits shared by all dogs, such as breed and fur color (characteristics), and the ability to bark and sit (behaviors).​
+
+# Terminology: Instance​
+# One can have an instance of a class or a particular object. The instance is the actual object created at runtime. In programmer jargon, the Lassie object is an instance of the Dog class. The set of values of the attributes of a particular object is called its state. The object consists of state and the behavior that's defined in the object's class.​
+
+# Terminology: Method​
+# An object's abilities. In language, methods are verbs. Lassie, being a Dog, has the ability to bark. So bark() is one of Lassie's methods. She may have other methods as well, for example sit() or eat() or walk() or save_timmy(). Within the program, using a method usually affects only one particular object; all Dogs can bark, but you need only one particular dog to do the barking​
+
+# Making our first Function in Python:
+# class PartyAnimal:
+#    x = 0
+#    name = ''
+#    def __init__(self, nam):
+#      self.name = nam
+#      print(self.name,'constructed')
+
+#    def party(self) :
+#      self.x = self.x + 1
+#      print(self.name,'party count',self.x)
+
+# stuff = list()
+# stuff.append('python')
+# stuff.append('chuck')
+# stuff.sort()
+# print (stuff[0])
+# print (stuff.__getitem__(0))
+# print (list.__getitem__(stuff,0))
